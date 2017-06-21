7ffc5729c756caec3dfa6fc1cbbb81cafebafa61
josephzeng <josephzeng36@gmail.com>
2017-06-21 23:38:18 +0800

    add logic push

---
+++ games/paigow/service/logic/push.lua
@@ -0,0 +1,16 @@
+-- Copyright (C) 2017 josephzeng <josephzeng36@gmail.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+local utils = require "utils"
+local skynet = require "skynet"
+local packet = require "packet"
+local msg = require "msg"
+
+local M = {}
+
+function M:send(fd, cmd, sessionid, param)
+end
+
+return M
